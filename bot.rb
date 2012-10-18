22e723807ae2e1c332e81416c20c9386d37e2c6a
Justin Bull <me@justinbull.ca>
2012-10-18 04:35:38 -0400

    4:30am very tired commit. It works. Very ugly, though. Brittle too.

--- bot.rb
+++ bot.rb
@@ -10,13 +10,32 @@ APP_VERSION = '0.1.0'
 
 MYBIKELANE_API_BASE_URL = 'http://www.mybikelane.com/api/'
 
-config = YAML.load_file('config.yml')
+MAX_ATTEMPTS = 3
 
+config = YAML.load_file('config.yml')
 
 def write_config_to_file(obj)
   output = File.new 'config.yml', 'w'
   output.puts YAML.dump(obj)
   output.close
+end
+
+def fire_tweet(status)
+  num_attempts = 0
+  begin
+    num_attempts += 1
+    Twitter.update status
+    puts "#{title_string}#{location_string}#{hashtag_string}#{record['url'].first}".green
+  rescue Twitter::Error::TooManyRequests => error
+    if num_attempts <= MAX_ATTEMPTS
+      # NOTE: Your process could go to sleep for up to 15 minutes but if you
+      # retry any sooner, it will almost certainly fail with the same exception.
+      sleep error.rate_limit.reset_in
+      retry
+    else
+      raise
+    end
+  end
 end
 
 # Do a check for OAuth. If not, fire the little wizzard
@@ -94,27 +113,46 @@ if config['twitter_configuration']['last_updated'].nil? or config['twitter_confi
 end
 
 # Grab the violation data
-puts "Making MyBikeLane API Call...".yellow
-begin
-  # records = XmlSimple.xml_in(Net::HTTP.get_response(URI("#{MYBIKELANE_API_BASE_URL}/posts?city_id=39&format=xml")).body)
-  records = XmlSimple.xml_in('test-data.xml')
-  # Sort the retrieved data in increasing order
-  records['record'].sort! { |a, b|  a['id'] <=> b['id'] }
-  records['record'].each do |record|
-    # Only allow records that are "later" than the last tweeted records to be, well, tweeted
-    if config['ignore_tweet_id_cache'] == true or config['last_id_tweeted'].nil? or record['id'].first > config['last_id_tweeted'] 
-      # puts "#{record['plate-state'].first.upcase} #{record['plate'].first.upcase} at #{record['location'].first}"
-      puts "#{record['title'].first} (#{record['location'].first}) #BikeTO #{record['url'].first}"
-      config['last_id_tweeted'] = record['id'].first
-    else
-      puts "Ignoring ID: #{record['occurred-at']}"
-    end
-  end
-  write_config_to_file config
-rescue Exception => e
-  puts "\033[1mMyBikeLane API call FAILED:\033[22m #{e.message}".red
-else
-  puts "Got API response OK!".green
-end
+loop do
+  config = YAML.load_file('config.yml')
+  puts "Making MyBikeLane API Call...".yellow
+  begin
+    records = XmlSimple.xml_in(Net::HTTP.get_response(URI("http://www.mybikelane.com/api/posts?city_id=39&format=xml")).body)
+    # records = XmlSimple.xml_in('test-data.xml')
+    # Sort the retrieved data in increasing order
+    records['record'].sort! { |a, b|  a['id'] <=> b['id'] }
+    records['record'].each do |record|
+      # Only allow records that are "later" than the last tweeted records to be, well, tweeted
+      if config['ignore_tweet_id_cache'] == true or config['last_id_tweeted'].nil? or record['id'].first > config['last_id_tweeted'] 
 
-# Twitter.update 'Testing Ruby-powered tweet! Holla at @f3ndot'
+        title_string = "#{record['title'].first} "
+        location_string = "(#{record['location'].first}) "
+        hashtag_string = '#BikeTO '
+        
+        character_space = 140
+        character_space -= [record['url'].first.length, config['twitter_configuration']['short_url_length']].min
+        character_space -= hashtag_string.length
+        character_space -= location_string.length
+
+        remaining_space = character_space - title_string.length
+
+        if remaining_space <= 0
+          title_string = "#{title_string[0..(remaining_space.abs+'...'.length)]}... "
+        end
+
+        fire_tweet "#{title_string}#{location_string}#{hashtag_string}#{record['url'].first}"
+
+        config['last_id_tweeted'] = record['id'].first
+      else
+        puts "Ignoring ID: #{record['occurred-at']}".red
+      end
+    end
+    write_config_to_file config
+  rescue Exception => e
+    puts "\033[1mMyBikeLane API call FAILED:\033[22m #{e.message}".red
+  else
+    puts "Got API response OK!".green
+  end
+  sleep(3600)
+  puts "------ SLEPT FOR AN HOUR. TWEETING NEW POSTS! ------".cyan
+end
